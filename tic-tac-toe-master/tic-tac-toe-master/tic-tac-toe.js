05bbdf97d3aeccaf33e23cfb1a36860604810070
Kekomeko <kaydiankkcampbell@gmail.com>
2022-10-08 15:27:04 -0500

    Made X's and O's

--- tic-tac-toe-master/tic-tac-toe-master/tic-tac-toe.js
+++ tic-tac-toe-master/tic-tac-toe-master/tic-tac-toe.js
@@ -1,8 +1,28 @@
 window.onload=function(){
- let squareDivs = document.getElementById("board").querySelectorAll("div");
- console.log(squareDivs);
+   gamePieces = [""];
+     let squareDivs = document.getElementById("board").querySelectorAll("div");
+       
 
- for(const item of squareDivs){
-    item.className = "square";
- }
-};
+      for(const item of squareDivs){
+          item.className = "square";
+      }
+
+      for(const item of squareDivs){
+          item.addEventListener("click", function(){
+             if (gamePieces[gamePieces.length-1] == "O" || gamePieces[gamePieces.length-1] == ""){
+               gamePieces.push ("X");
+               item.textContent=("X");
+               item.classList.add("X")
+               
+            }
+         
+            else if (gamePieces[gamePieces.length-1] == "X"){
+               gamePieces.push ("O");
+               item.textContent=("O");
+               item.classList.add("O")
+               
+            }
+         });
+      }
+   
+}
