993e641a9a86c9ac902a33684c1ce5e5c5563a8d
Kekomeko <kaydiankkcampbell@gmail.com>
2022-10-09 16:50:08 -0500

    Added logic to the game

--- tic-tac-toe-master/tic-tac-toe-master/tic-tac-toe.js
+++ tic-tac-toe-master/tic-tac-toe-master/tic-tac-toe.js
@@ -1,38 +1,78 @@
 window.onload=function(){
    gamePieces = [""];
-     let squareDivs = document.getElementById("board").querySelectorAll("div");
+   let squareDivs = document.getElementById("board").querySelectorAll("div");
+   let winner = document.getElementById("status");
+   let restart = document.querySelector("button");
        
 
-      for(const item of squareDivs){
-          item.className = "square"; 
-      }
-
-      for(const item of squareDivs){
-         item.addEventListener("click", function(){
-             if (gamePieces[gamePieces.length-1] == "O" || gamePieces[gamePieces.length-1] == ""){
-               gamePieces.push ("X");
-               item.textContent=("X");
-               item.classList.add("X")
-               
-            }
-         
-            else if (gamePieces[gamePieces.length-1] == "X"){
-               gamePieces.push ("O");
-               item.textContent=("O");
-               item.classList.add("O")
-            }
-         });
-          item.addEventListener("mouseenter", function(){
-            item.classList.add ("hover");
-
-         });
-         item.addEventListener("mouseleave", function(){
-            item.classList.remove ("hover");
-   
-         });
-
-         
-      }  
-
-
+   for(const item of squareDivs){
+      item.className = "square"; 
    }
+
+   for(const item of squareDivs){
+      item.addEventListener("click", function(){
+         if (gamePieces[gamePieces.length-1] == "O" || gamePieces[gamePieces.length-1] == ""){
+            gamePieces.push ("X");
+            item.textContent=("X");
+            item.classList.add("X")
+               
+         }
+         
+         else if (gamePieces[gamePieces.length-1] == "X"){
+            gamePieces.push ("O");
+            item.textContent=("O");
+            item.classList.add("O")
+         }
+         if ((squareDivs[0].textContent == "X" && squareDivs[1].textContent == "X" && squareDivs[2].textContent == "X")
+         ||(squareDivs[3].textContent == "X" && squareDivs[4].textContent == "X" && squareDivs[5].textContent == "X")
+         ||(squareDivs[6].textContent == "X" && squareDivs[7].textContent == "X" && squareDivs[8].textContent == "X")//rows
+         ||(squareDivs[0].textContent == "X" && squareDivs[3].textContent == "X" && squareDivs[6].textContent == "X")
+         ||(squareDivs[1].textContent == "X" && squareDivs[4].textContent == "X" && squareDivs[7].textContent == "X")
+         ||(squareDivs[2].textContent == "X" && squareDivs[5].textContent == "X" && squareDivs[8].textContent == "X")//columns
+         ||(squareDivs[0].textContent == "X" && squareDivs[4].textContent == "X" && squareDivs[8].textContent == "X")
+         ||(squareDivs[2].textContent == "X" && squareDivs[4].textContent == "X" && squareDivs[6].textContent == "X")//diagonal
+         ){
+            winner.textContent=("Congratulations! X is the Winner!");
+            console.log(winner);
+            winner.classList.add("you-won")
+         }
+         else if ((squareDivs[0].textContent == "O" && squareDivs[1].textContent == "O" && squareDivs[2].textContent == "O")
+         ||(squareDivs[3].textContent == "O" && squareDivs[4].textContent == "O" && squareDivs[5].textContent == "O")
+         ||(squareDivs[6].textContent == "O" && squareDivs[7].textContent == "O" && squareDivs[8].textContent == "O")//rows
+         ||(squareDivs[0].textContent == "O" && squareDivs[3].textContent == "O" && squareDivs[6].textContent == "O")
+         ||(squareDivs[1].textContent == "O" && squareDivs[4].textContent == "O" && squareDivs[7].textContent == "O")
+         ||(squareDivs[2].textContent == "O" && squareDivs[5].textContent == "O" && squareDivs[8].textContent == "O")//columns
+         ||(squareDivs[0].textContent == "O" && squareDivs[4].textContent == "O" && squareDivs[8].textContent == "O")
+         ||(squareDivs[2].textContent == "O" && squareDivs[4].textContent == "O" && squareDivs[6].textContent == "O")//diagonal
+         ){
+            winner.textContent=("Congratulations! O is the Winner!");
+            console.log(winner);
+            winner.classList.add("you-won")
+         }
+
+      });
+
+      item.addEventListener("mouseenter", function(){
+         item.classList.add ("hover");
+
+      });
+      item.addEventListener("mouseleave", function(){
+         item.classList.remove ("hover");
+
+      });
+      item.addEventListener
+      
+
+     
+      
+        
+   }
+
+         
+      
+}  
+                       
+
+         
+      
+   
